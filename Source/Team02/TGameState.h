b6f29a2dbdd8f8c70810467a1929eeac8a081e85
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-07-31 21:23:42 +0900

    add crossahir add GameTIme

--- Source/Team02/TGameState.h
+++ Source/Team02/TGameState.h
@@ -13,11 +13,23 @@ class TEAM02_API ATGameState : public AGameState
 
 public:
 	ATGameState();
-
 	virtual void BeginPlay() override;
+	
+	// 게임 시작 (초 단위)
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Time")
+	float GameTime;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Time")
+	float MaxGameTime;
+
+	//타이머
+	FTimerHandle GameTimerHandle; // 게임시간 감소용
+	FTimerHandle HUDUdpateTimerHandle; //UI 업데이트용
 
 	void StartLevel();
 	void EndLevel();
 	void UpdateHUD();
+	void UpdateGameTime();
 	
 };
