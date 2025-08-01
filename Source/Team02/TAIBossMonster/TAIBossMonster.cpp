d28e32d3eda3da1cc88e6da5d7215affe66cff13
seungcheol <dltmdcjfdls1@gmail.com>
2025-08-01 17:10:31 +0900

    Add anim

--- Source/Team02/TAIBossMonster/TAIBossMonster.cpp
+++ Source/Team02/TAIBossMonster/TAIBossMonster.cpp
@@ -1,4 +1,5 @@
 #include "Team02/TAIBossMonster/TAIBossMonster.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 ATAIBossMonster::ATAIBossMonster()
 {
@@ -9,5 +10,17 @@ ATAIBossMonster::ATAIBossMonster()
 void ATAIBossMonster::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (false==IsPlayerControlled())
+	{
+		bUseControllerRotationYaw = false;
+
+		GetCharacterMovement()->bOrientRotationToMovement = false;
+		GetCharacterMovement()->bUseControllerDesiredRotation = true;
+		GetCharacterMovement()->RotationRate = FRotator(0.f, 480.f, 0.f);
+
+		GetCharacterMovement()->MaxWalkSpeed = 300.f;
+		
+	}
 }
 
