fa5d92db6463a16117e2599a984b145c320d444c
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-08-01 11:06:30 +0900

    Merge branch 'Feature/InGameUI1' of https://github.com/NbcampUnreal/3rd_4th-Team2-CH3-Project into Feature/InGameUI1
    # Conflicts:
    #	Team02.sln.DotSettings.user

--- Source/Team02/TPlayerController.h
+++ Source/Team02/TPlayerController.h
@@ -17,7 +17,19 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	TSubclassOf<UUserWidget> PlayerUIWidgetClass;
 
+	// UI 위젯 참조 지정
+	UPROPERTY()
+	class UUserWidget* PlayerUIWidget;
+
+	// HP바 업데이트 함수
+	UFUNCTION(BlueprintCallable,Category="UI")
+	void UpdateHPBar();
+	
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
+
+	//UI 업데이트용 타이머
+	FTimerHandle UIUpdateTimerHandle;
 };
