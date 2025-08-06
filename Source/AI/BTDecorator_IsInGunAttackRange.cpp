320d0e829a82617f2dfc9504a0940f1921419fbc
Eldia <nsw22122@gmail.com>
2025-08-06 14:29:58 +0900

    fix TAIBossMonster, BTDecorator_IsINGunAttackRange

--- Source/AI/BTDecorator_IsInGunAttackRange.cpp
+++ Source/AI/BTDecorator_IsInGunAttackRange.cpp
@@ -1,6 +1,8 @@
 #include "AI/BTDecorator_IsInGunAttackRange.h"
 #include "AI/TAIController.h"
 #include "Character/TNonPlayerCharacter.h"
+#include "TAIBossMonster/TAIBossMonster.h"
+#include "TAIBossMonster/TBossAIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 const float UBTDecorator_IsInGunAttackRange::AttackRange(1000.f);
@@ -15,20 +17,41 @@ bool UBTDecorator_IsInGunAttackRange::CalculateRawConditionValue(UBehaviorTreeCo
 	bool bResult = Super::CalculateRawConditionValue(OwnerComp, NodeMemory);
 	checkf(bResult == true, TEXT("Super::CalculateRawConditionValue() function has returned false."));
 
-	ATAIController* AIController = Cast<ATAIController>(OwnerComp.GetAIOwner());
-	checkf(IsValid(AIController) == true, TEXT("Invalid AIController."))
+	ATAIController* AIController1 = Cast<ATAIController>(OwnerComp.GetAIOwner());
+	//checkf(IsValid(AIController1) == true, TEXT("Invalid AIController."))
 
-	ATNonPlayerCharacter* NPC = Cast<ATNonPlayerCharacter>(AIController->GetPawn());
-	checkf(IsValid(NPC) == true, TEXT("Invalid NPC."));
+	ATBossAIController* AIController2 = Cast<ATBossAIController>(OwnerComp.GetAIOwner());
+	//checkf(IsValid(AIController2)==true, TEXT("AIController is invalid"));
 
-	//타겟 플레이어 설정
-	ATCharacterBase* TargetPlayerCharacter = Cast<ATCharacterBase>(
-		OwnerComp.GetBlackboardComponent()->GetValueAsObject(ATAIController::TargetCharacterKey));
-
-	//타겟과 AI의 거리를 계산하고 AttackRange 안에 있으면 true 반환
-	if (IsValid(TargetPlayerCharacter) == true && TargetPlayerCharacter->IsPlayerControlled() == true)
+	
+	if (IsValid(AIController1) == true)
 	{
-		return NPC->GetDistanceTo(TargetPlayerCharacter) <= AttackRange;
+
+		ATNonPlayerCharacter* NPC = Cast<ATNonPlayerCharacter>(AIController1->GetPawn());
+		checkf(IsValid(NPC) == true, TEXT("Invalid NPC."));
+
+		//타겟 플레이어 설정
+		ATCharacterBase* TargetPlayerCharacter = Cast<ATCharacterBase>(
+			OwnerComp.GetBlackboardComponent()->GetValueAsObject(ATAIController::TargetCharacterKey));
+
+		//타겟과 AI의 거리를 계산하고 AttackRange 안에 있으면 true 반환
+		if (IsValid(TargetPlayerCharacter) == true && TargetPlayerCharacter->IsPlayerControlled() == true)
+		{
+			return NPC->GetDistanceTo(TargetPlayerCharacter) <= AttackRange;
+		}
+	}
+
+	if (IsValid(AIController2) == true)
+	{
+
+		ATAIBossMonster* NPC = Cast<ATAIBossMonster>(AIController2->GetPawn());
+		checkf(IsValid(NPC)==true, TEXT("NPC is invalid"));
+
+		ATCharacterBase* TargetPlayerCharacter = Cast<ATCharacterBase>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(ATBossAIController::TargetCharacterKey));
+		if (IsValid(TargetPlayerCharacter)==true && TargetPlayerCharacter->IsPlayerControlled()==true)
+		{
+			return NPC->GetDistanceTo(TargetPlayerCharacter) <= (AttackRange - 800);
+		}
 	}
 
 	return false;
