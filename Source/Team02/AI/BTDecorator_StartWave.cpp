123d25c305980e0e57885549b3c506902dd3275e
Eldia <nsw22122@gmail.com>
2025-08-08 00:29:51 +0900

    BTDecorator_StartWave의 웨이브 중인지 아닌지의 블래보드 값을 가져오게 설정

--- Source/Team02/AI/BTDecorator_StartWave.cpp
+++ Source/Team02/AI/BTDecorator_StartWave.cpp
@@ -1,6 +1,29 @@
 #include "Team02/AI/BTDecorator_StartWave.h"
 #include "AI/TAIController.h"
-#include "Character/TNonPlayerCharacter.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
+UBTDecorator_StartWave::UBTDecorator_StartWave()
+{
+	NodeName = TEXT("IsInWave");
+}
 
+bool UBTDecorator_StartWave::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	bool bResult = Super::CalculateRawConditionValue(OwnerComp, NodeMemory);
+	checkf(bResult == true, TEXT("Super::CalculateRawConditionValue() function has returned false."));
+
+	ATAIController* AIController = Cast<ATAIController>(OwnerComp.GetAIOwner());
+	checkf(IsValid(AIController) == true, TEXT("Invalid AIController."));
+	
+	UBlackboardComponent* BlackboardComponent = Cast<UBlackboardComponent>(AIController->GetBlackboardComponent());
+	checkf(IsValid(BlackboardComponent) == true, TEXT("Invalid BlackboardComponent."));
+
+	bool bIsInWave = BlackboardComponent->GetValueAsBool(ATAIController::IsInWaveKey);
+	
+	if (bIsInWave == true)
+	{
+		return true;
+	}
+
+	return false;
+}
